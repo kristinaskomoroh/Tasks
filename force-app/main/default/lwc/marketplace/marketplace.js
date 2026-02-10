2db7deb1152a74498abed6cddb7689e492279bed
kristinaskomoroh <kristina.skomoroh@vrpconsulting.com>
2026-02-10 12:34:01 +0300

    Refactored code

--- force-app/main/default/lwc/marketplace/marketplace.js
+++ force-app/main/default/lwc/marketplace/marketplace.js
@@ -3,25 +3,16 @@ import { LightningElement } from 'lwc';
 export default class Marketplace extends LightningElement {
     isJoinModalOpen = false;
     isSignModalOpen = false;
-
-     openModalJoin() {
-        console.log('Кнопка нажата!');
+    openModalJoin() {
         this.isJoinModalOpen = true; 
     }
-
     closeJoinModal() {
         this.isJoinModalOpen = false;
     }
-
     openModalSign(){
-         console.log('Кнопка нажата!');
         this.isSignModalOpen = true;
     }
-
     closeSignModal(){
         this.isSignModalOpen = false;
     }
-
-
-    
 }
